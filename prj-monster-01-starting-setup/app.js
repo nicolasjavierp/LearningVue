c294f447db8a0c21f18b9ac84962d1b0b74010b2
nicolas.pantazis <nicolas.pantazis@globant.com>
2023-06-29 09:45:50 -0300

    FInishing MonsterGame and learning proxies

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -8,7 +8,8 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             currentRound:0,
-            winner: null
+            winner: null,
+            logMessages: []
         };
     },
     // Actions = Methods
@@ -17,29 +18,53 @@ const app = Vue.createApp({
             this.currentRound++;
             const attackValue = getRandomValue(5,12);
             this.monsterHealth = this.monsterHealth - attackValue;
+            this.addLogMessage('player', 'attack', attackValue)
             this.attackPlayer();
+            this.addLogMessage('monster', 'attack', attackValue)
         },
         attackPlayer(){
             const attackValue = getRandomValue(8,15);
             this.playerHealth -= attackValue;
+            this.addLogMessage('monster', 'attack', attackValue)
         },
         specialAttackMonster(){
             this.currentRound++;
             const attackValue = getRandomValue(10,25);
             this.monsterHealth = this.monsterHealth - attackValue;
+            this.addLogMessage('player', 'Special attack', attackValue)
             this.attackPlayer();
+            this.addLogMessage('monster', 'attack', attackValue)
         },
         healPlayer(){
+            console.log("calling heal ! =")
             this.currentRound++;
             const healValue = getRandomValue(8,20);
+            
             if (this.playerHealth + healValue > 100){
                 this.playerHealth = 100
             }else{
                 this.playerHealth += healValue;
             }
+            this.addLogMessage('player', 'heal', healValue)
             this.attackPlayer();
         },
-        
+        startNewGame() {
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.winner = null;
+            this.currentRound = 0;
+            this.logMessages = []
+        },
+        surrender(){
+            this.winner= 'monster';
+        },
+        addLogMessage(who, what, value){
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value
+            });
+        }
     },
     computed:{
         monsterBarStyles(){
